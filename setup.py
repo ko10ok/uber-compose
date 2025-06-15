e276c7cc8e7e6599323ebb28e42c3561d7b4b001
Юрий Сагитов <yu.sagitov@2gis.ru>
2025-06-15 17:15:11 +0300

    test different python versions

--- setup.py
+++ setup.py
@@ -23,7 +23,7 @@ setup(
     long_description_content_type="text/markdown",
     author="Yuriy Sagitov",
     author_email="pro100.ko10ok@gmail.com",
-    python_requires=">=3.8.0",
+    python_requires=">=3.10.0",
     url="https://github.com/ko10ok/uber-compose",
     license="Apache-2.0",
     packages=find_packages(exclude=("tests",)),
@@ -31,11 +31,11 @@ setup(
     entry_points={},
     classifiers=[
         "License :: OSI Approved :: Apache Software License",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
+        "Programming Language :: Python :: 3.13",
+        "Programming Language :: Python :: 3.14",
     ],
     package_data={
         'uber_compose': ['version'],
