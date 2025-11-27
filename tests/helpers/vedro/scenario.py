6b441e276f21a20eabd068712b4d9cd6d78779bf
Yuriy Sagitov <pro100.ko10ok@gmail.com>
2025-11-27 10:44:17 +0300

    added filtering env test scenarios via separate param --uc-env

---
+++ tests/helpers/vedro/scenario.py
@@ -0,0 +1,33 @@
+from pathlib import Path
+from time import monotonic_ns
+
+from vedro import Scenario
+from vedro.core import VirtualScenario
+
+from uber_compose import Environment
+
+
+def make_scenario(env: Environment = None) -> VirtualScenario:
+    class _Scenario(Scenario):
+        __file__ = Path(f"scenario_{monotonic_ns()}.py").absolute()
+
+    if env:
+        _Scenario.env = env
+    return VirtualScenario(_Scenario, steps=[])
+
+
+def describe_scenario(scenario: VirtualScenario) -> dict:
+    env = getattr(scenario._orig_scenario, 'env', None)
+    env_desc = env.description if env else None
+
+    desc = f"Scenario(name={scenario._orig_scenario.__name__}"
+    if hasattr(scenario._orig_scenario, 'env'):
+        desc += f", env={scenario._orig_scenario.env}"
+    desc += ")"
+    return {
+        "name": scenario._orig_scenario.__name__,
+        "skipped": scenario.is_skipped(),
+        "env": env,
+        "env_desc": env_desc,
+        "description": repr(scenario),
+    }
